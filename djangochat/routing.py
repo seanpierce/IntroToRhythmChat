89e774380bbef5a8e3eac51f3bd4ee683ef0b7de
Sean Pierce <sumler.sean@gmail.com>
2020-04-29 20:20:27 -0700

    Initialize repo

---
+++ djangochat/routing.py
@@ -0,0 +1,15 @@
+# mysite/routing.py
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import chat.routing
+
+application = ProtocolTypeRouter({
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
